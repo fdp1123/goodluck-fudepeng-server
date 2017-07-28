08b99ea445521d2c372acc3a1446c070c74414ee
fudepeng <491031768@qq.com>
2017-07-28 18:42:01 +0800

    Initial commit

---
+++ src/main/java/com/bw/fudepeng/GoodluckFudepengServerApplication.java
@@ -0,0 +1,12 @@
+package com.bw.fudepeng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodluckFudepengServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoodluckFudepengServerApplication.class, args);
+	}
+}
